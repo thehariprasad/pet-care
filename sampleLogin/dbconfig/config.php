468148c9dd4f187a4c40a726c35eeac78b40d57b
Hari Prasad <hari@techondria.com>
2021-01-27 09:23:55 +0530

    Update config.php

--- sampleLogin/dbconfig/config.php
+++ sampleLogin/dbconfig/config.php
@@ -1,8 +1,15 @@
-<?php
-/*For My LocalPC*/
+/*<?php
 $con=mysqli_connect ("localhost", "root", "") or die ('I cannot connect to the database because: ' . mysql_error());
 mysqli_select_db ($con,'pet_care');
-?>
+?>*/
+<?php
 
+    $con = new mysqli("localhost","root","","pet_care");
+           if($con -> connect_errno > 0)
+    {
+        die ("Unable to connect to database : " . $con ->connect_error);
+    }
+
+?>
 
 
